c4c9da06a1f9ada64655e4ce37a6da20d0fbea46
Dave Gerrard <davetgerrard@gmail.com>
2012-04-30 11:36:37 +0100

    updated filter to retain uniprot ID

--- filter_iTraq.R
+++ filter_iTraq.R
@@ -1,6 +1,7 @@
+## Cliff Rowe's filtering script to select useable data columns from iTRAQ data files.
+## Tweaked by DTG
 
-
-H7rawdata<-read.csv(file="H7StemiTRAQ.csv") 
+H7rawdata<-read.csv(file="data/H7StemiTRAQ.csv") 
 
 H7<-H7rawdata[c(2,8,5,6,9,14,19,24,29,34,39)]
 
@@ -14,13 +15,14 @@ H7data<-rbind(H795[,3:11],H799[,3:11])	### dataset to use
 
 names(H7data)<-c("Accession", "Name","H7.1","H7_HLC.1","H7.2","H7_HLC.2","H7.3","H7_HLC.3","H7_HLC.4")
 
-H7data$Accession <- matrix(unlist(strsplit(as.character(H7data$Accession),"|",fixed=T)),ncol=3,byrow=T)[,2]
+H7data$Uniprot.Id <- matrix(unlist(strsplit(as.character(H7data$Accession),"|",fixed=T)),ncol=3,byrow=T)[,3]	# added by DTG to retain human readable unprot IDs
+H7data$Uniprot.Accession <- matrix(unlist(strsplit(as.character(H7data$Accession),"|",fixed=T)),ncol=3,byrow=T)[,2]
 
-write.csv(H7data,file="H7data.csv")
+write.csv(H7data,file="data/H7data.csv", row.names=F)
 ############################################
 
 
-H9rawdata<-read.csv(file="H9StemiTRAQ.csv") 
+H9rawdata<-read.csv(file="data/H9StemiTRAQ.csv") 
 
 H9<-H9rawdata[c(2,8,5,6,9,14,19,24,29,34,39)]
 
@@ -34,6 +36,7 @@ H9data<-rbind(H995[,3:11],H999[,3:11])  ### remove unsed and peptides columns
 
 names(H9data)<-c("Accession", "Name","H9.1","H9_HLC.1","H9.2","H9_HLC.2","H9.3","H9_HLC.3","H9_HLC.4")
 
-H9data$Accession <- matrix(unlist(strsplit(as.character(H9data$Accession),"|",fixed=T)),ncol=3,byrow=T)[,2]
+H9data$Uniprot.Id <- matrix(unlist(strsplit(as.character(H9data$Accession),"|",fixed=T)),ncol=3,byrow=T)[,3]	# added by DTG to retain human readable unprot IDs
+H9data$Uniprot.Accession <- matrix(unlist(strsplit(as.character(H9data$Accession),"|",fixed=T)),ncol=3,byrow=T)[,2]
 
-write.csv(H9data,file="H9data.csv")
+write.csv(H9data,file="data/H9data.csv", row.names=F)
