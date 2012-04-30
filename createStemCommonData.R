6097729f3d79287981bec770efc5c6b021525df8
Dave Gerrard <davetgerrard@gmail.com>
2012-04-30 11:34:41 +0100

    creating separate filter and merge scripts

---
+++ createStemCommonData.R
@@ -0,0 +1,16 @@
+#createStemCommonData.R
+
+
+H7data <- read.csv(file="data/H7data.csv")
+H9data <- read.csv(file="data/H9data.csv")
+
+
+stemcommon<-merge(H7data,H9data,incomparables=NA,all=TRUE)	# use the default 'by' to prevent duplicate columns
+stemcommondata<-stemcommon[complete.cases(stemcommon), ]
+
+nrow(stemcommon)
+nrow(stemcommondata)		# 1922 as per my method above.
+
+dataColumns <- c(grep('H7',names(stemcommondata)),grep('H9',names(stemcommondata)) )
+stemcommondata[1,dataColumns]
+
